f53abdb79df82cec00bb5f1e71e3ee0650cacab4
nguyenthitrang <nguyentrang.245.hn@gmail.com>
2016-04-03 03:57:32 +0000

    them like_count v picture cho comment

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160403022943) do
+ActiveRecord::Schema.define(version: 20160403035412) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "user_name"
     t.text     "body"
     t.integer  "idea_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+    t.integer  "like_count", default: 0
+    t.text     "picture"
+    t.text     "reply_id"
   end
 
   create_table "ideas", force: :cascade do |t|
